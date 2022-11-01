7bf7ebbcedc957552f8297834970306ba3b84036
wqskylar <wqskylar996@gmail.com>
2022-11-01 20:19:11 +0800

    1.0.0

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <h1>前台电商项目</h1>
+    <h2>作者：wq</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {},
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
