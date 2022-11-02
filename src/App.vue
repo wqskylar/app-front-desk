237992e5b562ff3caefb95c9257fb5b3b51e68af
wqskylar <wqskylar996@gmail.com>
2022-11-02 13:39:08 +0800

    v1.0.2

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,23 @@
   <div id="app">
     <h1>前台电商项目</h1>
     <h2>作者：wq</h2>
+    <Header></Header>
   </div>
 </template>
 
 <script>
+import Header from "./components/Header.vue";
+
 export default {
   name: "App",
-  components: {},
+  components: {
+    Header,
+  },
 };
 </script>
 
 <style>
+/* app统一设置 */
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -20,5 +26,18 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+/* 定义'icomoon'字体图标 */
+@font-face {
+  font-family: "icomoon";
+  src: url("assets/fonts/icomoon.eot?hspkx4");
+  src: url("assets/fonts/icomoon.eot?hspkx4#iefix") format("embedded-opentype"),
+    url("assets/fonts/icomoon.ttf?hspkx4") format("truetype"),
+    url("assets/fonts/icomoon.woff?hspkx4") format("woff"),
+    url("assets/fonts/icomoon.svg?hspkx4#icomoon") format("svg");
+  font-weight: normal;
+  font-style: normal;
+  font-display: block;
 }
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,4 +1,10 @@
-<template></template>
+<template>
+  <div>
+    <h1>使用字体图标</h1>
+    <!-- 定位字体图标 -->
+    <span style="font-family: icomoon; font-size: 20px"></span>
+  </div>
+</template>
 
 <script>
 export default {
